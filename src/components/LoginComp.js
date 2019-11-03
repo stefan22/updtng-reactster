cf91bdc1fc3925fd910eb13013b30b8b59eeafde
stefan22 <stefan.emailacct@gmail.com>
2019-11-03 10:45:49 +0000

    aside login-comp

--- src/components/LoginComp.js
+++ src/components/LoginComp.js
@@ -5,11 +5,11 @@ class LoginComp extends Component {
 
 
   render () {
-    console.log(this)
+
     return (
       <div id="main">
         <div id='edit-expense' className='dashboard edit-expense'>
-          <h3>Login comp</h3>
+          <h3>Login leave for after contact-us</h3>
 
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
           sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
