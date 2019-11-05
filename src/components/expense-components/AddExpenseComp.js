0980356dd9f9b4cd1fe8cc8b4bc9d39b0d7c8edb
stefan22 <stefan.emailacct@gmail.com>
2019-11-05 23:20:06 +0000

    add-exp-form

--- src/components/expense-components/AddExpenseComp.js
+++ src/components/expense-components/AddExpenseComp.js
@@ -5,6 +5,19 @@ import addXpImg from '../../images/axp.png'
 
 
 class AddExpenseComp extends Component {
+  constructor(props) {
+    super(props)
+    this.state ={
+      expense: {
+        name: '',
+        desc: '',
+        amount: 0,
+      },
+      errAmt: false,
+      errDesc: false,
+    }
+    this.handleSubmitExpense = this.handleSubmitExpense.bind(this)
+  }
 
   componentDidMount() {
     const axp = new TimelineMax()
@@ -18,9 +31,65 @@ class AddExpenseComp extends Component {
       })
   }
 
+  handleExpenseChange = (e) => {
+    const {name,value,type} = e.target
+    if(type === 'text') {
+      return (
+        this.setState({
+          expense: {
+            ...this.state.expense,
+            [name]: value,
+          }
+        })
+      )
+    }
+    if(type === 'textarea') {
+      console.log(value.length)
+      this.setState({
+        expense: {
+          ...this.state.expense,
+          [name]: value,
+        },
+        errDesc: (String(value.length) >= 3) ? false : true,
+      })
+    }
+    if(type === 'number') {
+      let val = String(value)
+      const regxp = /^[1-9]\d*(\.\d{0,2})?$/
+      let isMatch = !val || val.match(regxp)
+
+      if(isMatch) {
+        this.setState({
+          expense: {
+            ...this.state.expense,
+            [name]: Number(value)
+          },
+          errAmt: (Number(value) > 0) ? false : true,
+        })
+      }
+    }
+  }
+
+  handleSubmitExpense(e) {
+    e.preventDefault()
+    const {expense: {amount,desc}} = this.state
+    if(amount === 0) this.setState(() => ({errAmt: true}))
+    if(desc.length < 3) this.setState(() => ({errDesc: true}))
+    else {
+      this.setState(() => ({//clear values
+        expense:{
+          name: '',
+          amount: 0,
+          desc: '',
+        }}))
+      console.log('form submitted')
+    }
+  }
+
 
   render () {
 
+    const {expense:{name,desc,amount},errAmt,errDesc} = this.state
     return (
       <div id="main">
         <div id='add-expense' className='dashboard add-expense'>
@@ -29,33 +98,44 @@ class AddExpenseComp extends Component {
           <div className='addxp__form-wrap'>
 
             <form
-              onSubmit={this.handleSubmit}
+              onSubmit={this.handleSubmitExpense}
               id='add-expense__form'
             >
               <div className='addxp__inner-form'>
                 <div className='addxp__left'>
                   <h3>Fill out form to add expense.</h3>
-                Name:
+                  <h4>Name:</h4>
                   <input
+                    onChange={(e) => this.handleExpenseChange(e)}
                     className='addxp__input-field'
                     autoFocus
+                    required
                     type='text'
                     name='name'
+                    value={name}
                     placeholder='expense name'
                   />
 
-                Description:
+                  <h4>Description: { !!errDesc &&
+                    <span>{'An expense description is required'}</span>
+                  }</h4>
                   <textarea
+                    onChange={(e) => this.handleExpenseChange(e)}
+                    value={desc}
                     className='addxp__textarea-field'
                     rows={5}
                     cols={15}
-                    type='text'
-                    name='description'
+                    type='textarea'
+                    name='desc'
                     placeholder='expense description'
                   />
 
-                Amount:
+                  <h4>Amount: { !!errAmt &&
+                    <span>{'Please provide an amount value'}</span>
+                  }</h4>
                   <input
+                    onChange={(e) => this.handleExpenseChange(e)}
+                    value={amount}
                     className='addxp__input-field'
                     type='number'
                     name='amount'
