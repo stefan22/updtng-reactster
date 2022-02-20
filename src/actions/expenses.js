0a5fa0dcb8190dcead44c327e21e0e0ec4f0899f
stefan22 <stefanosemail@protonmail.com>
2022-02-20 04:05:45 +0000

    actions

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -1,26 +1,31 @@
-import uuid from 'uuid'
+import uuid from "uuid";
 
 //add expense
-export const addExpense = ({ name = '', desc = '', amount = 0, created = 0 } = {}) => ({
-  type: 'ADD_EXPENSE',
+export const addExpense = ({
+  name = "",
+  desc = "",
+  amount = 0,
+  created = 0,
+} = {}) => ({
+  type: "ADD_EXPENSE",
   expense: {
     id: uuid(),
     name,
     desc,
     amount,
     created,
-  }
-})
+  },
+});
 
 //edit expense
 export const editExpense = (id, updates) => ({
-  type: 'EDIT_EXPENSE',
+  type: "EDIT_EXPENSE",
   id,
   updates,
-})
+});
 
 //remove expense
 export const removeExpense = ({ id } = {}) => ({
-  type: 'REMOVE_EXPENSE',
-  id
-})
+  type: "REMOVE_EXPENSE",
+  id,
+});
--- src/actions/filters.js
+++ src/actions/filters.js
@@ -1,57 +1,57 @@
 //set text filter
-export const setTextFilter = (text='') => ({
-  type: 'SET_TEXT_FILTER',
+export const setTextFilter = (text = "") => ({
+  type: "SET_TEXT_FILTER",
   text,
-})
+});
 
 //set sortByDate
 export const sortByDate = () => ({
-  type: 'SORT_BY_DATE',
-})
+  type: "SORT_BY_DATE",
+});
 
 //set sortByAmount
 export const sortByAmount = () => ({
-  type: 'SORT_BY_AMOUNT',
-})
-
+  type: "SORT_BY_AMOUNT",
+});
 
 //set startDate
 export const setStartDate = (startDate) => ({
-  type: 'SET_START_DATE',
+  type: "SET_START_DATE",
   startDate,
-})
-
+});
 
 //set endDate
 export const setEndDate = (endDate) => ({
-  type: 'SET_END_DATE',
+  type: "SET_END_DATE",
   endDate,
-})
-
+});
 
 //filterBy :expenses
-export const filterBy = (expenses, {text,sortBy,startDate,endDate}) => {
-  return expenses.filter(itm => {
-    //created greater than startDate
-    const startDateMatch = typeof itm.startDate !== 'number' || itm.created >= startDate
-    //created less than endDate
-    const endDateMatch = typeof itm.endDate !== 'number' || itm.created <= endDate
-    //search by txt
-    const descMatch = itm.desc.toLowerCase().includes(text.toLowerCase())
-    const nameMatch = itm.name.toLowerCase().includes(text.toLowerCase())
-    //by name||desc
-    return (nameMatch || descMatch) && startDateMatch && endDateMatch
-  })
-  //sort date/amount
-    .sort((a,b) => {
-      if (sortBy === 'date') {
-        return a.created < b.created ? 1 : -1
-      }
-      else if(sortBy === 'amount') {
-        return a.amount < b.amount ? 1 : -1
-      }
-      else {
-        return expenses
-      }
-    })
-}
+export const filterBy = (expenses, { text, sortBy, startDate, endDate }) => {
+  return (
+    expenses
+      .filter((itm) => {
+        //created greater than startDate
+        const startDateMatch =
+          typeof itm.startDate !== "number" || itm.created >= startDate;
+        //created less than endDate
+        const endDateMatch =
+          typeof itm.endDate !== "number" || itm.created <= endDate;
+        //search by txt
+        const descMatch = itm.desc.toLowerCase().includes(text.toLowerCase());
+        const nameMatch = itm.name.toLowerCase().includes(text.toLowerCase());
+        //by name||desc
+        return (nameMatch || descMatch) && startDateMatch && endDateMatch;
+      })
+      //sort date/amount
+      .sort((a, b) => {
+        if (sortBy === "date") {
+          return a.created < b.created ? 1 : -1;
+        } else if (sortBy === "amount") {
+          return a.amount < b.amount ? 1 : -1;
+        } else {
+          return expenses;
+        }
+      })
+  );
+};
